62b0214b2453f4fe39522583fb77218f3d0892d7
Денис Александрович Федотов <dafedotov@dims.prv>
2023-11-03 14:41:44 +0300

    Initial version

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+int main()
+{
+    double op1, op2;
+    int ch;
+    std::cout<<"Please, enter the first operand: ";
+    std::cin>>op1;
+    std::cout<<"\nPlease, enter the second operand: ";
+    std::cin>>op2;
+    std::cout<<"\nPlease, choose the operation: ";
+    std::cout<<"\n1. Add"<<"\n2. Substract"<<"\n3. Multiply"<<"\n4. Divide"<<">";
+    std::cin>>ch;
+
+    switch (ch)
+    {
+        case 1: std::cout<<"Not ready yet."; break;
+        case 2: std::cout<<"Not ready yet."; break;
+        case 3: std::cout<<"Not ready yet."; break;
+        case 4: std::cout<<"Not ready yet."; break;
+    }
+
+    return 0;
+}
+
